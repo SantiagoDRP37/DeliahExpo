5ebb3ba17a4ed8eea78b3ef2137448b4a348c6a8
David Santiago Rojas Páez <santiagodrp37@gmail.com>
2020-06-29 23:45:20 -0500

    avance

--- servicios/servicios.js
+++ servicios/servicios.js
@@ -4,12 +4,54 @@ const server = express();
 const Sequelize = require('sequelize');
 const sequelize = require('../DataBase/bdConexion');
 
+const jwt = require('jsonwebtoken');
+
 server.get("/",(req,res)=>{
     res.send("Delilah Restó Api");
 });
+//autenticacion
+ const fima = 'estoEsmuySeguro123';
 
+function verficarTokenAdmin(req,res,next){
+  try {
+    const token = req.headears.authorization.split(' ')[1];
+    const descoficado = jwt.verify(token,firma);
+    console.log("aaa"+descoficado);
+    if (descoficado){
+      req.usuario = descoficado;
+      next()
+    }
+  } catch (error) {
+    res.json({error: 'error al validar token'});
+  }
+  
+}
+
+//----------------Login----------------------
+server.post("/Login",(req,res)=>{
+  const {
+    userNick,
+    contrasenea
+  } = req.body;
+  sequelize.authenticate().then(async()=>{
+
+      let query = `Select tipoUser FROM usuarios WHERE userNick = ${JSON.stringify(req.body.userNick)} AND contrasenea = ${JSON.stringify(req.body.contrasenea)}`;
+      const resultados = await sequelize.query(query,{type: sequelize.QueryTypes.SELECT});
+      console.log(resultados);
+      if(resultados.length > 0){
+        const token = jwt.sign({resultados},fima);
+        console.log("token: "+token);
+        res.json(token);
+      }else{
+        res.statusCode = 400;
+          res.send("Usuario o contraseña incorrecta.");
+      }
+      
+   });
+    
+});
 //----------------Usuarios----------------------
-server.get("/Usuario",(req,res)=>{
+server.get("/Usuario",verficarTokenAdmin,(req,res)=>{
     sequelize.authenticate().then(async()=>{
         const query = 'Select * FROM usuarios';
         const resultados = await sequelize.query(query,{type: sequelize.QueryTypes.SELECT});
@@ -47,7 +89,7 @@ server.post("/Usuario/Registrar",(req,res)=>{
                 telefono,
                 contrasenea,
                 tipoUser}});
-                res.status(200).json("usuario creado");
+                res.status(201).json("usuario creado");
           }
      });
 });
@@ -84,7 +126,7 @@ server.post("/Platos/Crear",(req,res)=>{
                 nombre,
                 precio,
                 foto}});
-                res.status(200).json("Plato creado");
+                res.status(201).json("Plato creado");
           }
      });
 });
@@ -111,6 +153,51 @@ server.delete("/Platos/EiminarPorId",(req,res)=>{
           res.send('El plato no existe en la base de datos');
         }
    });
+});
+server.patch("/Plato/ModificarPorId",(req,res)=>{
+  const{
+    idPlato,
+    variable,
+    valor
+  } = req.body;
+  sequelize.authenticate().then(async()=>{
+
+    let query = `SELECT idPlato FROM platos WHERE idPlato = ${JSON.stringify(req.body.idPlato)}`;
+    const resultados = await sequelize.query(query,{type: sequelize.QueryTypes.SELECT});
+    if (resultados.length > 0){
+      // variable
+      if (!variable){
+        res.statusCode = 404;
+          res.json({ error: "El proiedad de plato no encontrada"});
+      }else if(variable=="nombre"){
+        query = ` UPDATE platos SET nombre = :valor where idPlato = :idPlatoQUERY;`;
+        const updatePlato_nombre = await sequelize.query(query,{replacements:{
+          idPlatoQUERY: idPlato,
+          valor
+          }});
+          res.sendStatus(201);
+      }else if(variable=="precio"){
+        query = ` UPDATE platos SET precio = :valor where idPlato = :idPlatoQUERY;`;
+        const updatePlato_nombre = await sequelize.query(query,{replacements:{
+          idPlatoQUERY: idPlato,
+          valor
+          }});
+          res.sendStatus(201);
+      }else{
+        query = ` UPDATE platos SET foto = :valor where idPlato = :idPlatoQUERY;`;
+        const updatePlato_nombre = await sequelize.query(query,{replacements:{
+          idPlatoQUERY: idPlato,
+          valor
+          }});
+          res.sendStatus(201);
+      }
+     
+    }else{
+      res.statusCode = 404;
+          res.json({ error: " plato no encontrado"});
+    }
+  });
+ 
 });
 //----------------Pedidos-----------------------
 server.get("/Pedidos",(req,res)=>{
@@ -175,7 +262,7 @@ server.delete("/Pedido/EiminarPorId",(req,res)=>{
     sequelize.authenticate().then(async()=>{
       let query = `SELECT idPedido FROM pedidos WHERE idPedido = ${JSON.stringify(req.body.idPedido)}`;
       const resultados = await sequelize.query(query,{type: sequelize.QueryTypes.SELECT});
-      console.log(resultados);
+      
       if(resultados.length > 0 ){
           
           query = "delete from pedidos WHERE idPedido = :idPedido AND id_usuario = :id_usuario";
@@ -189,5 +276,28 @@ server.delete("/Pedido/EiminarPorId",(req,res)=>{
           res.send('la orden no existe en la base de datos');
         }
    });
+});
+server.patch("/Pedido/ModificarPorId",(req,res)=>{
+  const{
+    idPedido,
+    estado
+  } = req.body;
+  sequelize.authenticate().then(async()=>{
+
+    let query = `SELECT idPedido FROM pedidos WHERE idPedido = ${JSON.stringify(req.body.idPedido)}`;
+    const resultados = await sequelize.query(query,{type: sequelize.QueryTypes.SELECT});
+    if (resultados.length > 0){
+      query = ` UPDATE pedidos SET estado = :estadoPedido where idPedido = :idPedido;`;
+      const updatePedido = await sequelize.query(query,{replacements:{
+        estadoPedido : estado,
+        idPedido
+        }});
+        res.sendStatus(200);
+    }else{
+      res.statusCode = 404;
+          res.json({ error: "El pedido no existe"})
+    }
+  });
+ 
 });
 module.exports = server;
